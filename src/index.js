6b230c14f178a19e0f2e30bbf59d7cbec1e5440b
Brijesh <brijesh@perceptinfotech.com>
2022-12-21 14:36:27 +0530

    changes made in app.css and index.js

--- src/index.js
+++ src/index.js
@@ -23,7 +23,47 @@ const Index = (props) => {
         <Provider store={store}>
 
             <div styles={styles}>
-                <TnWebcamNew lang={lang} camids={camids} color={color} modid={modid} acclat={acclat} acclon={acclon}></TnWebcamNew>
+
+            <div className='tn-webcam-component'>
+                <h1 className='main-title'>TN-Webcam</h1>
+
+                <div className='tn-webcam-main'>
+                    <div className='tn-webcam-sidebar'></div>
+                    <div className='tn-webcam-details-area'>
+                    <div className='tn-webcam-slider'>
+                        <div className='tn-webcam-slider-top'>
+
+                        <div className='tn-webcam-slider-top-lt'>
+                            <div className='tn-webcam-logo'>
+                            <img src="https://www.walchsee.co/media/cms/walchsee.co/u/2022/04/walchsee-white.png" alt='logo' />
+                            </div>
+                            <div className='tn-webcam-place-logo'>
+                            <img src="https://www.walchsee.co/media/cms/walchsee.co/u/2022/04/walchsee-white.png" alt='logo' />
+                            </div>
+                        </div>
+
+                        <div className='tn-webcam-slider-top-rt'>
+                            <div className="tn-webcam-live">
+                            <span className="tn-webcam-live-dot"></span>
+                            <p className="tn-webcam-live-txt">live</p>
+                            </div>
+                            <div className="tn-webcam-zoom">
+                            <img src="https://webcamwidget.fullmarketing.at/assets/img/zoom.svg" alt="zoom" />
+                            </div>
+                        </div>
+
+                        </div>
+                        <div className='tn-webcam-slider-middle'>
+                            <TnWebcamNew lang={lang} camids={camids} color={color} modid={modid} acclat={acclat} acclon={acclon}></TnWebcamNew>
+                        </div>
+                        <div className='tn-webcam-slider-bottom'></div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+          
+                
+        
             </div>
 
         </Provider>
